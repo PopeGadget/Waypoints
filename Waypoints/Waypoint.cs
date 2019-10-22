a1e98f461e43354a6f62bca04811a5834190ba32
Joshua Wright <thejwright7080@gmail.com>
2019-10-22 04:48:33 +0100

    Update Waypoint.cs

--- Waypoints/Waypoint.cs
+++ Waypoints/Waypoint.cs
@@ -2,9 +2,8 @@
 {
     public class Waypoint
     {
-        /* Waypoints can either be a new addition
-         * or an attachment to the already placed
-         * signals and speed limit triggers. */
+        /* Waypoints can either be a new addition or an attachment to
+         * the already placed signals and speed limit triggers. */
 
         // Coordinates in 3D space.
         public double X { get; set; }
@@ -13,5 +12,10 @@
 
         // Speed limit for section of track directly after the waypoint.
         public int SpeedLimit { get; set; }
+        /* The value for the final waypoint shouldn't matter, but I
+         * imagine for the general case fetching the SpeedLimit value
+         * of the previous waypoint and copying it over would make
+         * the most sense, and change the value if one is specified
+         * whilst passing in the waypoints list. */
     }
 }
